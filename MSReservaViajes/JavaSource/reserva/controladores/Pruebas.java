7d52dfc766bc3c4444a259fd1c42668691ab8e54
Jesus <fra7jes7@gmail.com>
2020-04-07 12:13:33 -0500

    Se creo una clase de pruebas

---
+++ MSReservaViajes/JavaSource/reserva/controladores/Pruebas.java
@@ -0,0 +1,14 @@
+package reserva.controladores;
+
+public class Pruebas {
+
+	public Pruebas() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
